2cacfc860580379fdf71e33cc0de73f7f4414324
YOGENDRA K <yogendragowdak006@gmail.com>
2021-07-26 09:49:37 +0530

    printing string in reverse order

---
+++ ChatBot/ReverseString/ReverseString.java
@@ -0,0 +1,15 @@
+package ChatBot.ReverseString;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String in1 = sc.nextLine();
+        String in2 = sc.nextLine();
+        String in3 = sc.nextLine();
+        System.out.println(in3 + "\n" + in2 + "\n" + in1);
+        sc.close();
+    }
+}
---
+++ ChatBot/ReverseString/readme.md
@@ -0,0 +1,11 @@
+Print strings in the reverse order
+Write a program that reads three strings and outputs them in the reverse order, each in a new line.
+Sample Input:
+Java
+Programming
+Language
+
+Sample Output:
+Language
+Programming
+Java
