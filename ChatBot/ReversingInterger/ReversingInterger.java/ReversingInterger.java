319d5ec7d5151ebce8b9b776f6a28c2335aca02c
YOGENDRA K <yogendragowdak006@gmail.com>
2021-07-26 17:57:40 +0530

    printing given interger in reverse order

---
+++ ChatBot/ReversingInterger/ReversingInterger.java/ReversingInterger.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int a = -1;
+        System.out.println( a++);
+        int number = 12241;
+        int digit = ((number % 100) / 10) % 10;
+        System.out.println(digit);
+        int num = scanner.nextInt();
+        int last = num % 10;
+        int middle = (num % 100) / 10;
+        int first = (num % 1000) / 100;
+        if(last != 0) {
+            System.out.print(last + "" + middle + "" + first);
+        } else {
+            System.out.print(middle + "" + first);
+        }scanner.close();
+    }
+}
---
+++ ChatBot/ReversingInterger/ReversingInterger.md/ReversingInterger.md
@@ -0,0 +1,16 @@
+Reversing
+Write a program that reads a three-digit number, calculates the new number by reversing its digits, and outputs a new number.
+
+
+Sample Input:
+320
+
+Sample Output:
+23
+
+
+Sample Input:
+976
+
+Sample Output:
+679
