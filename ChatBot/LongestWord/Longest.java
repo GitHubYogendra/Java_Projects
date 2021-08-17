7984889807893453ea77a0922ad3c125bee257d9
GitHubYogendra <yogendragowdak006@gmail.com>
2021-08-17 18:56:42 +0530

    The longest word

---
+++ ChatBot/LongestWord/Longest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Longest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] array = str.split(" ");
+        int maxLength = 0;
+        String longestString = null;
+        for (String s : array) {
+            if (s.length() > maxLength) {
+                maxLength = s.length();
+                longestString = s;
+            }
+        }
+        System.out.println(longestString);
+    }
+}
+
+
---
+++ ChatBot/LongestWord/Readme.md
@@ -0,0 +1,18 @@
+The longest word
+In the given string find the first longest word and output it.
+
+Input data
+
+Given a string in a single line. Words in the string are separated by a single space.
+
+Output data
+
+Output the longest word. If there are several such words, you should output the one, which occurs earlier.
+
+
+Sample Input:
+one two three four five six
+
+Sample Output:
+three
+
