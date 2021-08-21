b6c80fab02d3171a3eccc186e231a4e9d1c1c1af
GitHubYogendra <yogendragowdak006@gmail.com>
2021-08-21 11:50:56 +0530

    Sum array elements greater than a value

---
+++ ChatBot/SumArrayGreater/SumGreater.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class SumGreater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] array = new int[num];
+        int sum = 0;
+        for (int i = 0; i < num; i++) {
+            array[i] = sc.nextInt();
+        }
+        int n = sc.nextInt();
+        for (int i = 0;i < num;i++){
+            if (n < array[i]) {
+                sum = sum + array[i];
+            }
+        }
+        System.out.println("\n" + sum);
+    }
+}
